1368bb6af84cc004ddae8305ead4666e1683ddbd
JSHEN9085 <jshen9085@gmail.com>
2019-09-29 21:59:25 -0400

    method overload

--- MethodOverload/src/com/jshen/Main.java
+++ MethodOverload/src/com/jshen/Main.java
@@ -3,6 +3,30 @@ package com.jshen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int newScore1 = calculateScore("Sam", 100);
+        System.out.println("new score " + newScore1);
+        int newScore2 = calculateScore(200);
+        System.out.println("new socre " + newScore2);
+
     }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score){
+        System.out.println("unnamed player scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("No player and no score");
+        return 0;
+    }
+
+//    public static void calculateScore(){
+//        System.out.println("No player and no score");
+//    }
+//    error: same method name, same parameter, different return type
 }
